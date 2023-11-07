cbd6e4e01595df591e1356bf68b88df7687bb518
Arun Kushwaha <er.arunkushwaha@gmail.com>
2023-11-07 22:08:16 +0545

    editor changed

--- src/contentScript/JobFrom.tsx
+++ src/contentScript/JobFrom.tsx
@@ -1,30 +1,204 @@
-import React, { useMemo, useRef } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { useDebounce } from "use-debounce";
 
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import {
+  BtnBold,
+  BtnItalic,
+  Editor,
+  EditorProvider,
+  Toolbar,
+  BtnUnderline,
+} from "react-simple-wysiwyg";
+
 import InputBox from "../component/InputBox";
 
 const JobFrom = (props: any) => {
-  const modules = {
-    toolbar: [["bold", "italic", "underline"]],
+  const [companyName, setCompanyName] = useState<string>("");
+  const [jobsTitle, setJobstitle] = useState<string>("");
+  const [companyLocation, setCompanyLocation] = useState<string>("");
+  const [jobDescription, setJobDescription] = useState<any>("");
+  const [postUrl, setPostUrl] = useState<string>("");
+  const [activeUrl, setActiveUrl] = useState<string>(window.location.href);
+  const [debounceValue] = useDebounce(activeUrl, 3000);
+  const targetElementRef = useRef();
+
+  const getContentFromLinkedInJobs = (): void => {
+    try {
+      setPostUrl(window.location.href);
+
+      const jobsBody = document?.getElementsByClassName(
+        "job-details-jobs-unified-top-card__job-title"
+      );
+      if (jobsBody[0]) {
+        setJobstitle(jobsBody[0]?.textContent.trim());
+      }
+
+      setTimeout(() => {
+        let jobDetailsElement = document?.getElementById("job-details");
+
+        const about = jobDetailsElement?.querySelector("span");
+
+        setJobDescription(about?.innerHTML);
+      }, 500);
+
+      // Find the first <span> element inside the jobDetailsElement
+
+      const location = document.getElementsByClassName(
+        "job-details-jobs-unified-top-card__bullet"
+      );
+      if (location[0]) {
+        setCompanyLocation(location[0]?.textContent?.trim());
+      }
+
+      // Assuming you have a reference to the DOM element
+      setTimeout(() => {
+        const domElement = document?.querySelector(
+          ".jobs-unified-top-card.t-14"
+        );
+
+        const aTag = domElement?.querySelector("a.app-aware-link");
+        const companyName = aTag?.textContent;
+        setCompanyName(companyName?.trim());
+      }, 500);
+    } catch (error) {
+      console.log("error---", error);
+    }
   };
 
-  const {
-    companyName,
-    setCompanyName,
-    jobsTitle,
-    setJobstitle,
-    companyLocation,
-    setCompanyLocation,
-    postUrl,
-    setPostUrl,
-    jobDescription,
-    setJobDescription,
-  } = props;
+  const getJobsFromIndeed = (): void => {
+    setPostUrl(window.location.href);
 
-  const style = {
-    height: "150px",
+    setTimeout(() => {
+      const titleElement = document.querySelector(
+        ".jobsearch-JobInfoHeader-title"
+      );
+      // Get the text content from the titleElement
+      const text = titleElement?.textContent?.trim();
+      // Extract "React.js Developer"
+      const jobTitle = text?.split(" - ")[0];
+      setJobstitle(jobTitle);
+    }, 1000);
+
+    // Get the HTML element by its data-testid attribute
+    const locationElement = document.querySelector(
+      '[data-testid="inlineHeader-companyLocation"]'
+    );
+    if (locationElement) {
+      // Get the text content from the element
+      const location = locationElement.textContent.trim();
+      setCompanyLocation(location);
+    }
+
+    const companyElement = document.getElementsByClassName(
+      "css-1f8zkg3 e19afand0"
+    );
+    if (companyElement) {
+      setCompanyName(companyElement[0]?.textContent);
+    }
+
+    const about = document.getElementById("jobDescriptionText");
+    setJobDescription(about?.innerHTML);
   };
+
+  const getJobsFromDice = (): void => {
+    setPostUrl(window.location.href);
+    // Get the HTML element by its data-cy attribute
+    const titleElement = document.querySelector('[data-cy="jobTitle"]');
+    if (titleElement) {
+      // Get the text content from the element
+      const title = titleElement?.textContent?.trim();
+      setJobstitle(title);
+    }
+    const companyNameEle = document.querySelector(
+      '[data-cy="companyNameLink"]'
+    );
+    if (companyNameEle) {
+      // Get the text content from the element
+      const companyName = companyNameEle?.textContent?.trim();
+      setCompanyName(companyName);
+    }
+    // Get the HTML element by its data-testid attribute
+    const locationElement = document.querySelector(
+      '[data-cy="locationDetails"]'
+    );
+    if (locationElement) {
+      // Get the text content from the element
+      const location = locationElement?.textContent?.trim();
+      setCompanyLocation(location);
+    }
+
+    const jobDescriptionEle = document.querySelector(
+      '[data-testid="jobDescriptionHtml"]'
+    );
+    if (jobDescriptionEle) {
+      // Get the text content from the element
+      const description = jobDescriptionEle?.innerHTML;
+      setJobDescription(description);
+    }
+  };
+
+  const getJobFrozipRecuriter = (): void => {
+    setPostUrl(window.location.href);
+
+    const titleEle = document.querySelector(".u-mv--remove.u-textH2");
+    const title = titleEle?.textContent?.trim();
+    setJobstitle(title);
+
+    const companyEle = document.querySelector(".text-primary.text-large");
+    const companyName = companyEle?.textContent?.trim();
+    setCompanyName(companyName);
+
+    const locationEle = document.querySelector(".text-primary.text-large");
+    const location = locationEle?.textContent?.trim();
+    setCompanyLocation(location);
+
+    const jobDescriptionEle = document.querySelector(".job-body");
+    if (jobDescriptionEle) {
+      const description = jobDescriptionEle?.innerHTML;
+      setJobDescription(description);
+    }
+  };
+
+  useEffect(() => {
+    if (window.location.href.includes("linkedin.")) {
+      getContentFromLinkedInJobs();
+    }
+    if (window.location.href.includes("indeed.")) {
+      getJobsFromIndeed();
+    }
+    if (window.location.href.includes("dice.")) {
+      getJobsFromDice();
+    }
+    if (window.location.href.includes("ziprecruiter.")) {
+      getJobFrozipRecuriter();
+    }
+  }, [debounceValue]);
+
+  useEffect(() => {
+    const observer = new MutationObserver(() => {
+      const url = window.location.href;
+      // chrome.runtime.sendMessage({ action: "urlChange", url });
+      if (url !== activeUrl) {
+        setActiveUrl(url);
+      }
+    });
+
+    // Observe changes in the DOM
+    observer.observe(document, { childList: true, subtree: true });
+  }, []);
+  // const {
+  //   companyName,
+  //   setCompanyName,
+  //   jobsTitle,
+  //   setJobstitle,
+  //   companyLocation,
+  //   setCompanyLocation,
+  //   postUrl,
+  //   setPostUrl,
+  //   jobDescription,
+  //   setJobDescription,
+  // } = props;
+
   return (
     <div className="job__detail__container">
       <div className="job_detail_header"> Jobs Hunter </div>
@@ -51,12 +225,18 @@ const JobFrom = (props: any) => {
         <div className="job_input_section">
           <span className="job_box_title">Description </span>
           <div className="scrollbar-container">
-            {/* <ReactQuill
-              theme="snow"
-              value={jobDescription}
-              onChange={setJobDescription}
-              modules={modules}
-            /> */}
+            <EditorProvider>
+              <Editor
+                value={jobDescription}
+                onChange={(e) => setJobDescription(e.target.value)}
+              >
+                <Toolbar>
+                  <BtnBold />
+                  <BtnItalic />
+                  <BtnUnderline />
+                </Toolbar>
+              </Editor>
+            </EditorProvider>
           </div>
         </div>
       </div>
--- src/contentScript/contentScript.tsx
+++ src/contentScript/contentScript.tsx
@@ -7,179 +7,6 @@ import JobFrom from "./JobFrom";
 
 const App: React.FC<{}> = () => {
   const [showFrom, setShowFrom] = useState<boolean>(false);
-  const [companyName, setCompanyName] = useState<string>("");
-  const [jobsTitle, setJobstitle] = useState<string>("");
-  const [companyLocation, setCompanyLocation] = useState<string>("");
-  const [jobDescription, setJobDescription] = useState<any>("");
-  const [postUrl, setPostUrl] = useState<string>("");
-  const [activeUrl, setActiveUrl] = useState<string>(window.location.href);
-  const [debounceValue] = useDebounce(activeUrl, 3000);
-  const targetElementRef = useRef();
-
-  const getContentFromLinkedInJobs = (): void => {
-    try {
-      setPostUrl(window.location.href);
-
-      const jobsBody = document?.getElementsByClassName(
-        "job-details-jobs-unified-top-card__job-title"
-      );
-      if (jobsBody[0]) {
-        setJobstitle(jobsBody[0]?.textContent.trim());
-      }
-
-      setTimeout(() => {
-        let jobDetailsElement = document?.getElementById("job-details");
-
-        const about = jobDetailsElement?.querySelector("span");
-
-        setJobDescription(about?.innerHTML);
-      }, 500);
-
-      // Find the first <span> element inside the jobDetailsElement
-
-      const location = document.getElementsByClassName(
-        "job-details-jobs-unified-top-card__bullet"
-      );
-      if (location[0]) {
-        setCompanyLocation(location[0]?.textContent?.trim());
-      }
-
-      // Assuming you have a reference to the DOM element
-      setTimeout(() => {
-        const domElement = document?.querySelector(
-          ".jobs-unified-top-card.t-14"
-        );
-
-        const aTag = domElement?.querySelector("a.app-aware-link");
-        const companyName = aTag?.textContent;
-        setCompanyName(companyName?.trim());
-      }, 500);
-    } catch (error) {
-      console.log("error---", error);
-    }
-  };
-
-  const getJobsFromIndeed = (): void => {
-    setPostUrl(window.location.href);
-
-    setTimeout(() => {
-      const titleElement = document.querySelector(
-        ".jobsearch-JobInfoHeader-title"
-      );
-      // Get the text content from the titleElement
-      const text = titleElement?.textContent?.trim();
-      // Extract "React.js Developer"
-      const jobTitle = text?.split(" - ")[0];
-      setJobstitle(jobTitle);
-    }, 1000);
-
-    // Get the HTML element by its data-testid attribute
-    const locationElement = document.querySelector(
-      '[data-testid="inlineHeader-companyLocation"]'
-    );
-    if (locationElement) {
-      // Get the text content from the element
-      const location = locationElement.textContent.trim();
-      setCompanyLocation(location);
-    }
-
-    const companyElement = document.getElementsByClassName(
-      "css-1f8zkg3 e19afand0"
-    );
-    if (companyElement) {
-      setCompanyName(companyElement[0]?.textContent);
-    }
-
-    const about = document.getElementById("jobDescriptionText");
-    setJobDescription(about?.innerHTML);
-  };
-
-  const getJobsFromDice = (): void => {
-    setPostUrl(window.location.href);
-    // Get the HTML element by its data-cy attribute
-    const titleElement = document.querySelector('[data-cy="jobTitle"]');
-    if (titleElement) {
-      // Get the text content from the element
-      const title = titleElement?.textContent?.trim();
-      setJobstitle(title);
-    }
-    const companyNameEle = document.querySelector(
-      '[data-cy="companyNameLink"]'
-    );
-    if (companyNameEle) {
-      // Get the text content from the element
-      const companyName = companyNameEle?.textContent?.trim();
-      setCompanyName(companyName);
-    }
-    // Get the HTML element by its data-testid attribute
-    const locationElement = document.querySelector(
-      '[data-cy="locationDetails"]'
-    );
-    if (locationElement) {
-      // Get the text content from the element
-      const location = locationElement?.textContent?.trim();
-      setCompanyLocation(location);
-    }
-
-    const jobDescriptionEle = document.querySelector(
-      '[data-testid="jobDescriptionHtml"]'
-    );
-    if (jobDescriptionEle) {
-      // Get the text content from the element
-      const description = jobDescriptionEle?.innerHTML;
-      setJobDescription(description);
-    }
-  };
-
-  const getJobFrozipRecuriter = (): void => {
-    setPostUrl(window.location.href);
-
-    const titleEle = document.querySelector(".u-mv--remove.u-textH2");
-    const title = titleEle?.textContent?.trim();
-    setJobstitle(title);
-
-    const companyEle = document.querySelector(".text-primary.text-large");
-    const companyName = companyEle?.textContent?.trim();
-    setCompanyName(companyName);
-
-    const locationEle = document.querySelector(".text-primary.text-large");
-    const location = locationEle?.textContent?.trim();
-    setCompanyLocation(location);
-
-    const jobDescriptionEle = document.querySelector(".job-body");
-    if (jobDescriptionEle) {
-      const description = jobDescriptionEle?.innerHTML;
-      setJobDescription(description);
-    }
-  };
-
-  useEffect(() => {
-    if (window.location.href.includes("linkedin.")) {
-      getContentFromLinkedInJobs();
-    }
-    if (window.location.href.includes("indeed.")) {
-      getJobsFromIndeed();
-    }
-    if (window.location.href.includes("dice.")) {
-      getJobsFromDice();
-    }
-    if (window.location.href.includes("ziprecruiter.")) {
-      getJobFrozipRecuriter();
-    }
-  }, [debounceValue]);
-
-  useEffect(() => {
-    const observer = new MutationObserver(() => {
-      const url = window.location.href;
-      // chrome.runtime.sendMessage({ action: "urlChange", url });
-      if (url !== activeUrl) {
-        setActiveUrl(url);
-      }
-    });
-
-    // Observe changes in the DOM
-    observer.observe(document, { childList: true, subtree: true });
-  }, []);
 
   return (
     <div className="content__script__section">
@@ -196,17 +23,17 @@ const App: React.FC<{}> = () => {
       </div>
       {showFrom && (
         <JobFrom
-          companyName={companyName}
-          setCompanyName={setCompanyName}
-          jobsTitle={jobsTitle}
-          setJobstitle={setJobstitle}
-          companyLocation={companyLocation}
-          setCompanyLocation={setCompanyLocation}
-          postUrl={postUrl}
-          setPostUrl={setPostUrl}
-          targetElementRef={targetElementRef}
-          jobDescription={jobDescription}
-          setJobDescription={setJobDescription}
+        // companyName={companyName}
+        // setCompanyName={setCompanyName}
+        // jobsTitle={jobsTitle}
+        // setJobstitle={setJobstitle}
+        // companyLocation={companyLocation}
+        // setCompanyLocation={setCompanyLocation}
+        // postUrl={postUrl}
+        // setPostUrl={setPostUrl}
+        // targetElementRef={targetElementRef}
+        // jobDescription={jobDescription}
+        // setJobDescription={setJobDescription}
         />
       )}
     </div>
