987c3998233dad2ce7673e1e402e6801c34342a3
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-01-25 15:28:51 +0545

    button decorated

--- src/contentScript/contentScript.tsx
+++ src/contentScript/contentScript.tsx
@@ -53,14 +53,14 @@ const App: React.FC<{}> = () => {
         // Set attributes and styles for the button
         beautifulButton.href = jobLink;
         beautifulButton.target = "_blank";
-        beautifulButton.textContent = "Add This jobs to Careerai";
+        beautifulButton.textContent = "Add This Job to CareerAi";
         beautifulButton.style.display = "inline-block";
-        beautifulButton.style.padding = "10px 20px";
+        beautifulButton.style.padding = "7px 20px";
         beautifulButton.style.backgroundColor = "#0145FD";
         beautifulButton.style.color = "#ffffff";
         beautifulButton.style.textDecoration = "none";
         beautifulButton.style.borderRadius = "5px";
-        beautifulButton.style.fontSize = "18px";
+        beautifulButton.style.fontSize = "16px";
         beautifulButton.style.fontWeight = "500";
         beautifulButton.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.1)";
         beautifulButton.style.transition = "background-color 0.3s ease";
