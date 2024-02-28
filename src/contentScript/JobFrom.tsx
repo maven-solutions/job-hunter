0b9978726e95d8be8c89fe87b9166ad45e728edd
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-02-28 13:31:54 +0545

    glassdoor code split

--- src/contentScript/JobFrom.tsx
+++ src/contentScript/JobFrom.tsx
@@ -10,6 +10,7 @@ import CloseIcon from "../component/CloseIcon";
 import SaveButton from "../component/SaveButton";
 import { getJobFromBuiltin } from "../jobExtractor/builtin";
 import { getJobFromSimplyhiredLandingPage } from "../jobExtractor/simplyhired";
+import { getJobFromGlassdoor } from "../jobExtractor/glassdoor";
 
 const JobFrom = (props: any) => {
   const { setShowForm } = props;
@@ -250,14 +251,6 @@ const JobFrom = (props: any) => {
     setSource("Dice");
   };
 
-  const removeRatingFromEnd = (inputString) => {
-    // Remove the rating at the end of the string
-    let stringWithoutRating = inputString.replace(/\d+(\.\d+)?★$/, "");
-    // Remove any trailing whitespace
-    let finalString = stringWithoutRating.trim();
-    return finalString;
-  };
-
   // Example usage
 
   const getJobsFromZipRecuriter1 = (zipDom: any) => {
@@ -319,77 +312,6 @@ const JobFrom = (props: any) => {
     setJobType(null);
     setEasyApply(null);
     setSource("Ziprecruiter");
-  };
-
-  const getJobFromGlassdoor = (): void => {
-    setPostUrl(window.location.href);
-
-    // this is for the desing where there is tab section in that page
-    const titleElement = document.querySelector('[data-test="job-title"]');
-
-    if (titleElement) {
-      // Get the text content from the element
-      const title = titleElement?.textContent?.trim();
-      setJobstitle(title);
-    }
-
-    const companyNameEle = document.querySelector('[data-testid="detailText"]');
-
-    if (companyNameEle) {
-      // Get the text content from the element
-      const inputString = companyNameEle?.textContent?.trim();
-      const companyName = removeRatingFromEnd(inputString);
-      setCompanyName(companyName);
-    }
-
-    setEmployment(null);
-    setJobType(null);
-    setPostedDate("n/a");
-    setEasyApply(null);
-
-    const jobDescriptionEle = document.querySelector(".css-1vbe1p2");
-    if (jobDescriptionEle) {
-      // Get the text content from the element
-      const description = jobDescriptionEle?.innerHTML;
-      setJobDescription(description);
-    }
-
-    // end of   desing where there is tab section in that page
-
-    //  this is for where ther is no tabs in listing page
-
-    const titleElement2 = document.querySelector(".JobDetails_jobTitle__Rw_gn");
-
-    if (titleElement2) {
-      // Get the text content from the element
-      const title = titleElement2?.textContent?.trim();
-      if (title) {
-        setJobstitle(title);
-      }
-    }
-
-    const companyNameEle2 = document.querySelector(
-      ".EmployerProfile_employerName__8w0tV"
-    );
-
-    if (companyNameEle2) {
-      // Get the text content from the element
-      const companyName = companyNameEle2?.textContent?.trim();
-      if (companyName) {
-        setCompanyName(companyName);
-      }
-    }
-
-    const jobDescriptionEle2 = document.querySelector(
-      ".JobDetails_jobDescription__6VeBn"
-    );
-    if (jobDescriptionEle2) {
-      // Get the text content from the element
-      const description = jobDescriptionEle2?.innerHTML;
-      setJobDescription(description);
-    }
-
-    setSource("Glassdoor");
   };
 
   const getJobFromSimplyhired = (): void => {
@@ -472,7 +394,17 @@ const JobFrom = (props: any) => {
       window.location.href.includes("glassdoor.") &&
       window.location.href.includes("job-listing")
     ) {
-      getJobFromGlassdoor();
+      getJobFromGlassdoor(
+        setPostUrl,
+        setJobstitle,
+        setJobDescription,
+        setPostedDate,
+        setSource,
+        setCompanyName,
+        setEmployment,
+        setJobType,
+        setEasyApply
+      );
     }
 
     if (
