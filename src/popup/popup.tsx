ed122ebb807e73d9df1a4bf1a7e12219e27c7996
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-05-05 20:32:45 +0545

    all done

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import Store from "../store/store";
-import JobDetector from "../landingPage/DetectJob/JobDetector";
-import "./index.css";
 
 const App: React.FC<{}> = () => {
   // console.log("popupfired::");
