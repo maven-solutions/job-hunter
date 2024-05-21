e30d8089a720c4927346e707a3cc64d80f35bc96
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-05-21 15:02:40 +0545

    link type added

--- src/autofill/FromFiller/urlTypeDataFiller.ts
+++ src/autofill/FromFiller/urlTypeDataFiller.ts
@@ -4,7 +4,11 @@ import { fieldNames } from "./fieldsname";
 
 export const urlTypeDataFiller = (tempDiv: any, applicantData: Applicant) => {
   // Extract input fields of type "text"
-  const textInputFields = tempDiv.querySelectorAll('input[type="url"]');
+  let textInputFields = tempDiv.querySelectorAll('input[type="url"]');
+  if (!textInputFields || textInputFields?.length === 0) {
+    textInputFields = tempDiv.querySelectorAll('input[type="link"]');
+  }
+
   textInputFields.forEach((input, index) => {
     // console.log(input);
     // Extract all attributes
@@ -27,8 +31,33 @@ export const urlTypeDataFiller = (tempDiv: any, applicantData: Applicant) => {
         handleValueChanges(input);
         return true; // Stop iterating
       }
+      if (
+        checkIfExist(labelText, fieldNames.github) ||
+        checkIfExist(attribute.value, fieldNames.github)
+      ) {
+        input.value = applicantData.github_url;
+        input.focus(); // Autofocus on the input field
+        input.click();
+        input.select();
+        handleValueChanges(input);
+        return true; // Stop iterating
+      }
+      if (
+        checkIfExist(labelText, fieldNames.portfolio) ||
+        checkIfExist(attribute.value, fieldNames.portfolio)
+      ) {
+        input.value = applicantData.portfolio_url;
+        input.focus(); // Autofocus on the input field
+        input.click();
+        input.select();
+        handleValueChanges(input);
+        return true; // Stop iterating
+      }
       return false; // Continue iterating
     });
+
     // You can do whatever you want with each input field here
   });
+
+  // for
 };
