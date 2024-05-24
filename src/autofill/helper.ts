ad0a4aa775adbac66f6ca2bc087bfeab0fb25d80
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-05-24 13:21:43 +0545

    icims solved

--- src/autofill/helper.ts
+++ src/autofill/helper.ts
@@ -118,7 +118,7 @@ export const detectInputAndFillData = async (
     fancyRadiotypeFiller(tempDiv ?? tempDivForFile, applicantData);
     dateTypeDataFiller(tempDiv ?? tempDivForFile, applicantData);
     checkboxTypeDataFiller(tempDiv ?? tempDivForFile, applicantData);
-    fileTypeDataFiller(tempDivForFile, applicantData, false);
+    fileTypeDataFiller(tempDivForFile, applicantData, true);
     selectDataExtract(tempDiv ?? tempDivForFile, applicantData, false);
     customSelectFiller(tempDiv ?? tempDivForFile, applicantData, false);
     customSelectFiller2(tempDiv ?? tempDivForFile, applicantData, false);
