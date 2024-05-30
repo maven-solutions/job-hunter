381cad72f281e683828c013d9d308d31053af5ae
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-05-30 10:01:03 +0545

    live url added

--- src/config/urlconfig.ts
+++ src/config/urlconfig.ts
@@ -1,11 +1,15 @@
 export const BASE_URL = "https://d2fa6tipx2eq6v.cloudfront.net/api/v1";
 export const VA_BASE_URL = "https://d2fa6tipx2eq6v.cloudfront.net/api/v1/va";
 
+// export const BASE_URL = "https://backend.careerai.io/api/v1";
+// export const VA_BASE_URL = "https://backend.careerai.io/api/v1/va";
+
 export const STAGING_WEBSITE_URL = "https://resumebuilder.joinswiftly.com";
 export const LIVE_WEBSITE_URL = "https://app.careerai.io";
 
+//
 // state handler
 // two state should be always false
-export const EXTENSION_IN_LOCAL = false;
-export const EXTENSION_IN_STAGING = true;
+export const EXTENSION_IN_LOCAL = true;
+export const EXTENSION_IN_STAGING = false;
 export const EXTENSION_IN_LIVE = false;
