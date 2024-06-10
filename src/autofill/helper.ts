aafc90453cd47efef7545974ec01df8a3ea8f6d0
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-06-10 14:40:46 +0545

    jcat added

--- src/autofill/helper.ts
+++ src/autofill/helper.ts
@@ -34,6 +34,7 @@ import { oraclecloud } from "./domainSpecific/oraclecloud";
 import { ultipro } from "./domainSpecific/ultipro";
 import { workable } from "./domainSpecific/workable";
 import { hiretrakstar } from "./domainSpecific/hiretrakstar";
+import { jcat } from "./domainSpecific/jcat";
 
 export const setLocalStorageData = (key: any, value: any): void => {
   chrome.storage.local.set({
@@ -259,6 +260,9 @@ export const detectInputAndFillData = async (
     }
     if (window.location.href.includes(".hire.trakstar.")) {
       await hiretrakstar(tempDiv ?? tempDivForFile, applicantData);
+    }
+    if (window.location.href.includes(".jcat.")) {
+      await jcat(tempDiv ?? tempDivForFile, applicantData);
     }
 
     if (window.location.href.includes(".ashbyhq.")) {
