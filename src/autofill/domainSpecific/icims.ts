197f99a5be3ff6d7cd0b1f484dc398f6443bc3de
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-06-11 15:51:44 +0545

    time reduced in icims

--- src/autofill/domainSpecific/icims.ts
+++ src/autofill/domainSpecific/icims.ts
@@ -5,13 +5,14 @@ export const icims = async (tempDiv: any, applicantData: Applicant) => {
   const acceptButton = tempDiv.querySelector('[for="accept_gdpr"]');
   if (acceptButton) {
     acceptButton.click();
+    await delay(1000);
   }
-  await delay(1000);
+
   const nextButton = tempDiv.querySelector(
     'input[id="enterEmailSubmitButton"]'
   );
   if (nextButton) {
     nextButton.click();
+    await delay(1000);
   }
-  await delay(1000);
 };
