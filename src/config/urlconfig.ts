f8fda5dfa34ee8e4125d75288b6a10738751b406
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-06-11 16:16:41 +0545

    urlconfig added

--- src/config/urlconfig.ts
+++ src/config/urlconfig.ts
@@ -1,18 +1,14 @@
+// export const BASE_URL = "https://d2fa6tipx2eq6v.cloudfront.net/api/v1";
+// export const VA_BASE_URL = "https://d2fa6tipx2eq6v.cloudfront.net/api/v1/va";
+
+export const BASE_URL = "https://backend.careerai.io/api/v1";
+export const VA_BASE_URL = "https://backend.careerai.io/api/v1/va";
+
 export const STAGING_WEBSITE_URL = "https://resumebuilder.joinswiftly.com";
 export const LIVE_WEBSITE_URL = "https://app.careerai.io";
 
 // state handler
 // two state should be always false
 export const EXTENSION_IN_LOCAL = false;
-export const EXTENSION_IN_STAGING = true;
-export const EXTENSION_IN_LIVE = false;
-
-export const BASE_URL = EXTENSION_IN_LIVE
-  ? "https://backend.careerai.io/api/v1"
-  : "https://d2fa6tipx2eq6v.cloudfront.net/api/v1";
-export const VA_BASE_URL = EXTENSION_IN_LIVE
-  ? "https://backend.careerai.io/api/v1/va"
-  : "https://d2fa6tipx2eq6v.cloudfront.net/api/v1/va";
-
-// export const BASE_URL = "https://backend.careerai.io/api/v1";
-// export const VA_BASE_URL = "https://backend.careerai.io/api/v1/va";
+export const EXTENSION_IN_STAGING = false;
+export const EXTENSION_IN_LIVE = true;
