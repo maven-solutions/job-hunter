89c7c7af8d34011a49945b5473f64b603633a797
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-06-14 16:18:21 +0545

    code cleaning

--- src/landingPage/DetectJob/JobDetector.tsx
+++ src/landingPage/DetectJob/JobDetector.tsx
@@ -1,36 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "./index.css";
-import {
-  glassDoorNotiification,
-  simplyHiredNotiification,
-} from "../../component/InfoNotification";
-import {
-  addButtonToGlassdoorWebsite,
-  addButtonToSimplyHired,
-} from "../../component/CareerAibutton";
 import Logo from "../../component/Logo";
 import LoginFrom from "../../auth/LoginForm/LoginFrom";
-
 import DisplayJob from "../../page/displayJob/DisplayJob";
 import Profile from "../../page/profile/Profile";
 import JobDetail from "../../page/jobDetail/JobDetail";
 import MenuPopUp from "../../component/menuPopup/MenuPopUp";
 import { RootStore, useAppDispatch, useAppSelector } from "../../store/store";
-import { getContentFromLinkedInJobs } from "../../jobExtractor/Linkedin";
 import {
   EXTENSION_ACTION,
   SHOW_PAGE,
   SUPPORTED_WEBSITE,
 } from "../../utils/constant";
 import {
-  clearJobState,
   clearStageData,
   setButtonDisabledFalse,
   setJobReqSucccessFalse,
 } from "../../store/features/JobDetail/JobDetailSlice";
-import { getJobFromSimplyhired } from "../../jobExtractor/SimplyHired";
-import { getJobsFromDice } from "../../jobExtractor/Dice";
-import { getJobsFromIndeed } from "../../jobExtractor/Indeed";
 
 import Builtin from "../../jobExtractor/Builtin";
 import Glassdoor from "../../jobExtractor/Glassdoor";
@@ -44,7 +30,6 @@ import {
   chekJobExists,
   getApplicationStageData,
 } from "../../store/features/JobDetail/JobApi";
-import { getJobFromZipRecruiter } from "../../jobExtractor/Ziprecuriter";
 import { setResumeResponseToFalse } from "../../store/features/ResumeList/ResumeListSlice";
 import useWebsiteDetection from "../../hooks/useWebsiteDetection";
 import {
@@ -57,9 +42,6 @@ import useSimplyhiredGlassdoorNoti from "../../hooks/useSimplyhiredGlassdoorNoti
 
 const JobDetector = (props: any) => {
   const { content, popup } = props;
-
-  // const [showIcon, setShowIcon] = useState<boolean>(false);
-  // const [showAutofillPage, setShowAutofillPage] = useState<boolean>(false);
   const [postUrl, setPostUrl] = useState<string>("");
   const [website, setWebsite] = useState<string>("");
   const [showPage, setShowPage] = useState<string>("");
@@ -107,16 +89,30 @@ const JobDetector = (props: any) => {
   };
 
   useEffect(() => {
-    // Function to remove the element
+    localStorage.removeItem("url");
+    const observer = new MutationObserver(() => {
+      const url = window.location.href;
+      // chrome.runtime.sendMessage({ action: "urlChange", url });
+      if (url !== postUrl) {
+        setPostUrl(url);
+      }
+    });
 
+    // Observe changes in the DOM
+    observer.observe(document, { childList: true, subtree: true });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
     // Function to observe changes in the DOM
     function observeModal() {
       const modalObserver = new MutationObserver((mutations) => {
         let majorChangeDetected = false;
         let workdaysButtonChanged = false;
-        // let modal: any = "";
-        // let mainContent: any = "";
-        // let stadlone: any = "";
+
         mutations.forEach((mutation) => {
           if (mutation.type === "childList") {
             // Check if the modal is added
@@ -153,17 +149,6 @@ const JobDetector = (props: any) => {
         }
       });
 
-      const progressBar = document.querySelector(
-        '[data-automation-id="progressBar"]'
-      );
-      if (progressBar) {
-        // Start observing the progressBar for changes
-        modalObserver.observe(progressBar, {
-          childList: true,
-          subtree: true,
-        });
-      }
-
       // Start observing the body for changes
       modalObserver.observe(document.body, {
         childList: true,
@@ -189,23 +174,6 @@ const JobDetector = (props: any) => {
     }
   }, [jobDetailState.check_job_res_success]);
 
-  useEffect(() => {
-    localStorage.removeItem("url");
-    const observer = new MutationObserver(() => {
-      const url = window.location.href;
-      // chrome.runtime.sendMessage({ action: "urlChange", url });
-      if (url !== postUrl) {
-        setPostUrl(url);
-      }
-    });
-
-    // Observe changes in the DOM
-    observer.observe(document, { childList: true, subtree: true });
-
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
   const loadUser = () => {
     chrome.storage.local.get(["ci_user"]).then((result) => {
       const data = result.ci_user;
@@ -244,72 +212,9 @@ const JobDetector = (props: any) => {
       // return true;
     });
   }, []);
-  // useEffect(() => {
-  //   chrome.runtime.onMessage.addListener(async function (
-  //     request,
-  //     sender,
-  //     sendResponse
-  //   ) {
-  //     if (request.message === "updateFields") {
-  //       // detectInputAndFillData(request.data);
-  //     }
-  //   });
-  // }, []);
 
-  // useEffect(() => {
-  //   let intervalId: any = "";
-  //   if (
-  //     window.location.href.includes("glassdoor") &&
-  //     !window.location.href.includes("job-listing")
-  //   ) {
-  //     glassDoorNotiification();
-  //     // Clear any existing intervals before setting a new one
-  //     intervalId = setInterval(addButtonToGlassdoorWebsite, 3000);
-  //   }
-  //   if (window.location.href === "https://www.simplyhired.com/") {
-  //     simplyHiredNotiification();
-  //     intervalId = setInterval(addButtonToSimplyHired, 3000);
-  //   }
-  //   // Clear the interval when the component unmounts
-  //   return () => clearInterval(intervalId);
-  // }, []);
   useSimplyhiredGlassdoorNoti();
   useTrackJobsFromWebsite(dispatch, setShowPage);
-  // useEffect(() => {
-  //   if (window.location.href.includes("linkedin.")) {
-  //     setTimeout(() => {
-  //       getContentFromLinkedInJobs(dispatch);
-  //     }, 2000);
-  //   }
-  //   if (window.location.href.includes("indeed.")) {
-  //     setTimeout(() => {
-  //       getJobsFromIndeed(dispatch);
-  //     }, 2000);
-  //   }
-
-  //   if (
-  //     window.location.href.includes("dice.") &&
-  //     window.location.href.includes("job-detail")
-  //   ) {
-  //     setTimeout(() => {
-  //       getJobsFromDice(dispatch);
-  //     }, 2000);
-  //   }
-
-  //   if (window.location.href.includes("ziprecruiter.")) {
-  //     setTimeout(() => {
-  //       getJobFromZipRecruiter(dispatch);
-  //     }, 2000);
-  //   }
-
-  //   if (window.location.href.includes("simplyhired.")) {
-  //     setTimeout(() => {
-  //       getJobFromSimplyhired(dispatch);
-  //     }, 2000);
-  //   }
-  //   setShowPage("");
-  //   dispatch(clearJobState());
-  // }, [window.location.href]);
 
   return (
     <div className="content__script__section">
