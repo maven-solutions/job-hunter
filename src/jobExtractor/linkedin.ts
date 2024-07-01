243e020dd5ae6ed162cd08e60456189606a4540d
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-07-01 13:49:22 +0545

    code is copyed

--- src/jobExtractor/linkedin.ts
+++ src/jobExtractor/linkedin.ts
@@ -1,3 +1,94 @@
+const getCompanyDetails = (dispatch) => {
+  const companyDetails: CompanyDetails = {};
+
+  const companyDetailsEle =
+    document.querySelector<HTMLElement>(".jobs-company__box");
+  if (!companyDetailsEle) {
+    return;
+  }
+
+  // Get company logo
+  const logo = companyDetailsEle.querySelector<HTMLImageElement>("img")?.src;
+  if (logo) {
+    companyDetails.logo = logo;
+  }
+
+  // Get company name and link
+  const atag = companyDetailsEle.querySelector<HTMLAnchorElement>(
+    ".artdeco-entity-lockup__content a"
+  );
+  if (atag?.textContent) {
+    companyDetails.name = atag.textContent.trim();
+  }
+  if (atag?.href) {
+    companyDetails.link = `https://www.linkedin.com${atag.getAttribute(
+      "href"
+    )}`;
+  }
+
+  // Get company summary
+  const summarySection =
+    companyDetailsEle.querySelector<HTMLElement>(".t-14.mt5");
+  if (summarySection?.textContent) {
+    const formattedSummary = summarySection.textContent
+      .split("\n")
+      .map((part) => part.trim())
+      .filter((part) => part !== "")
+      .join(" • ");
+    companyDetails.summary = formattedSummary;
+  }
+
+  // Get company description
+  const desc = companyDetailsEle.querySelector<HTMLElement>(
+    ".jobs-company__company-description > *:first-child"
+  );
+  if (desc?.innerHTML) {
+    const sanitizedDescription = sanitizeHtml(desc.innerHTML);
+    if (sanitizedDescription) {
+      companyDetails.description = sanitizedDescription;
+    }
+  }
+  //   dispatch(setCompanyDetails(companyDetails));
+};
+
+const getHiringTeamDetails = (dispatch) => {
+  let recruiterDetails: RecruiterDetails = {};
+
+  const hiringSectionEle = document.querySelector(
+    ".hirer-card__hirer-information"
+  );
+
+  if (!hiringSectionEle) {
+    return;
+  }
+
+  const nameTag = hiringSectionEle.querySelector<HTMLElement>(
+    ".jobs-poster__name strong"
+  );
+  if (nameTag?.textContent) {
+    recruiterDetails.name = nameTag.textContent.trim();
+  }
+
+  const prfileLink = hiringSectionEle.querySelector<HTMLAnchorElement>("a");
+  if (prfileLink?.href) {
+    recruiterDetails.link = prfileLink.href;
+  }
+
+  const profilesection = hiringSectionEle.previousElementSibling;
+  const prfileImage = profilesection.querySelector<HTMLImageElement>("img");
+  if (prfileImage?.src) {
+    recruiterDetails.profileImage = prfileImage?.src;
+  }
+
+  const detailsEle = hiringSectionEle.querySelector<HTMLElement>(
+    ".hirer-card__hirer-information .text-body-small"
+  );
+  if (detailsEle?.textContent) {
+    recruiterDetails.title = detailsEle.textContent.trim();
+  }
+  //   dispatch(setRecruiterDetails(recruiterDetails));
+};
+
 export const getContentFromLinkedInJobs = (
   setPostUrl,
   clearStateAndCity,
