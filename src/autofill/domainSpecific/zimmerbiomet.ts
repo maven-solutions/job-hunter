dc70838701db52a9f63fd6c65ad5c4e53fb7d252
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-07-05 17:22:12 +0545

    more filles added in zimmer

--- src/autofill/domainSpecific/zimmerbiomet.ts
+++ src/autofill/domainSpecific/zimmerbiomet.ts
@@ -85,10 +85,115 @@ const isAdult = async (applicantData: Applicant) => {
     }
   });
 };
+const fillGender = async (applicantData: Applicant) => {
+  const select: any = document.querySelector(
+    '[aria-describedby="identityContent.custgender"]'
+  );
+  if (!select) {
+    return;
+  }
+
+  Array.from(select.options).find((option: any) => {
+    if (
+      fromatStirngInLowerCase(option?.text) ===
+      fromatStirngInLowerCase(applicantData.gender)
+    ) {
+      option.selected = true;
+      handleValueChanges(select);
+      return true;
+    }
+  });
+};
+const hispanic = async (applicantData: Applicant) => {
+  const select: any = document.querySelector(
+    '[aria-describedby="identityContent.ethnicity"]'
+  );
+  if (!select) {
+    return;
+  }
+
+  Array.from(select.options).find((option: any) => {
+    if (
+      fromatStirngInLowerCase(option?.text) === "hispanicorlatino" &&
+      applicantData.hispanic_or_latino
+    ) {
+      option.selected = true;
+      handleValueChanges(select);
+      return true;
+    }
+
+    if (
+      fromatStirngInLowerCase(option?.text) === "nothispaniceorlatino" &&
+      !applicantData.hispanic_or_latino
+    ) {
+      option.selected = true;
+      handleValueChanges(select);
+
+      return true;
+    }
+  });
+};
+
+const fillRace = async (applicantData: Applicant) => {
+  const select: any = document.querySelector(
+    '[aria-describedby="identityContent.race"]'
+  );
+  if (!select) {
+    return;
+  }
+
+  Array.from(select.options).find((option: any) => {
+    if (
+      fromatStirngInLowerCase(option?.text) &&
+      fromatStirngInLowerCase(option?.text).includes(
+        fromatStirngInLowerCase(applicantData?.race)
+      )
+    ) {
+      option.selected = true;
+      handleValueChanges(select);
+      return true;
+    }
+  });
+};
+
+const FillDisability = async (applicantData: Applicant) => {
+  const select: any = document.querySelector(
+    '[aria-describedby="identityContent.custVetStatus]'
+  );
+  console.log("dis::", select);
+  if (!select) {
+    return;
+  }
+
+  Array.from(select.options).find((option: any) => {
+    if (
+      fromatStirngInLowerCase(option?.text).includes("yes") &&
+      applicantData.disability_status
+    ) {
+      option.selected = true;
+      handleValueChanges(select);
+      return true;
+    }
+
+    if (
+      fromatStirngInLowerCase(option?.text).includes("no") &&
+      !applicantData.disability_status
+    ) {
+      option.selected = true;
+      handleValueChanges(select);
+
+      return true;
+    }
+  });
+};
 export const zimmerbiomet = async (tempDiv: any, applicantData: Applicant) => {
   let tempDivForFile = document.querySelector("body");
   await fileTypeDataFiller(tempDivForFile, applicantData, false);
   await fillAuthorization(applicantData);
   await fillSposership(applicantData);
   await isAdult(applicantData);
+  await fillGender(applicantData);
+  await hispanic(applicantData);
+  await fillRace(applicantData);
+  await FillDisability(applicantData);
 };
