e7d7cd7fe787efd0f97c785d1099b846d556d374
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-07-15 11:27:38 +0545

    simply hired

--- src/jobExtractor/dice.ts
+++ src/jobExtractor/dice.ts
@@ -12,7 +12,6 @@ export const getJobsFromDice = (
   setEmployment,
   setSource,
   setCompanyName,
-
   setJoboverview,
   setLocation
 ): void => {
--- src/jobExtractor/simplyhired.ts
+++ src/jobExtractor/simplyhired.ts
@@ -1,1 +1,54 @@
-export const getJobFromSimplyhiredLandingPage = () => {};
+const getJobFromSimplyhired = (
+  setPostUrl,
+  setJobstitle,
+  setJobDescription,
+  setPostedDate,
+  setEasyApply,
+  setJobType,
+  setEmployment,
+  setSource,
+  setCompanyName,
+  setCity,
+  setState,
+  setJoboverview,
+  setLocation
+): void => {
+  setPostUrl(window.location.href);
+  // this is for the desing where there is tab section in that page
+  const titleElement = document.querySelector('[data-testid="viewJobTitle"]');
+
+  if (titleElement) {
+    // Get the text content from the element
+    const title = titleElement?.textContent?.trim();
+    setJobstitle(title);
+  }
+
+  const companyNameEle = document.querySelector(
+    // '[data-test="employer-name"]'
+    '[data-testid="detailText"]'
+  );
+
+  if (companyNameEle) {
+    // Get the text content from the element
+    const inputString = companyNameEle?.textContent?.trim();
+    setCompanyName(inputString);
+  }
+
+  setEmployment(null);
+  setJobType(null);
+  setPostedDate("n/a");
+  setCity("n/a");
+  setState("n/a");
+  setEasyApply(null);
+
+  const jobDescriptionEle = document.querySelector(
+    '[data-testid="viewJobBodyJobFullDescriptionContent"]'
+  );
+  if (jobDescriptionEle) {
+    // Get the text content from the element
+    const description = jobDescriptionEle?.innerHTML;
+    setJobDescription(description);
+  }
+
+  setSource("Simplyhired");
+};
