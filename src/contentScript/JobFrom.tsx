996b3f8aa2360b03175875593809e06cdfe58634
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-07-15 11:33:18 +0545

    indeed added

--- src/contentScript/JobFrom.tsx
+++ src/contentScript/JobFrom.tsx
@@ -13,6 +13,7 @@ import { getJobFromBuiltin } from "../jobExtractor/builtin";
 import { getJobFromGlassdoor } from "../jobExtractor/glassdoor";
 import { getContentFromLinkedInJobs } from "../jobExtractor/linkedin";
 import { getJobsFromDice } from "../jobExtractor/dice";
+import { getJobsFromIndeed } from "../jobExtractor/indeed";
 
 const JobFrom = (props: any) => {
   const { setShowForm } = props;
@@ -86,41 +87,6 @@ const JobFrom = (props: any) => {
     // Check if the created date is valid and the original string is not empty
     return !isNaN(date.getTime()) && !isNaN(Date.parse(str));
   }
-
-  const getJobsFromIndeed = (): void => {
-    setPostUrl(window.location.href);
-    clearStateAndCity();
-
-    setTimeout(() => {
-      const titleElement = document?.querySelector(
-        ".jobsearch-JobInfoHeader-title"
-      );
-      // Get the text content from the titleElement
-      const text = titleElement?.textContent?.trim();
-      // Extract "React.js Developer"
-      const jobTitle = text?.split(" - ")[0];
-      if (jobTitle) {
-        setJobstitle(jobTitle);
-      }
-    }, 1000);
-
-    const companyElement = document.querySelector(
-      '[data-testid="inlineHeader-companyName"]'
-    );
-
-    if (companyElement) {
-      setCompanyName(companyElement?.textContent.trim());
-    }
-
-    const about = document.getElementById("jobDescriptionText");
-    setJobDescription(about?.innerHTML);
-
-    setJobType(null);
-    setEmployment(null);
-    setPostedDate("n/a");
-    setEasyApply(null);
-    setSource("Indeed");
-  };
 
   // Example usage
 
@@ -240,7 +206,20 @@ const JobFrom = (props: any) => {
       );
     }
     if (window.location.href.includes("indeed.")) {
-      getJobsFromIndeed();
+      getJobsFromIndeed(
+        setPostUrl,
+        clearStateAndCity,
+        setJobstitle,
+        setJobDescription,
+        setPostedDate,
+        setEasyApply,
+        setJobType,
+        setEmployment,
+        setSource,
+        setCompanyName,
+        setJoboverview,
+        setLocation
+      );
     }
     if (window.location.href.includes("dice.")) {
       getJobsFromDice(
