87cdc40f8a8482920f23d7fffcb588505605af86
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-07-22 15:30:38 +0545

    avature modified

--- src/autofill/domainSpecific/avature.ts
+++ src/autofill/domainSpecific/avature.ts
@@ -1,7 +1,7 @@
 import { Applicant } from "../data";
-import { delay, fromatStirngInLowerCase } from "../helper";
+import { delay, fromatStirngInLowerCase, handleValueChanges } from "../helper";
 
-const fillEligibility = (applicantData: Applicant) => {
+const fillEligibility = async (applicantData: Applicant) => {
   const allLabel = document.querySelectorAll(
     'label[class="datasetfieldLabel"]'
   );
@@ -41,7 +41,7 @@ const fillEligibility = (applicantData: Applicant) => {
   }
 };
 
-const fillVisa = (applicantData: Applicant) => {
+const fillVisa = async (applicantData: Applicant) => {
   const allLabel = document.querySelectorAll('label[class="fieldLabel"]');
   for (const singleField of allLabel) {
     const questionField = singleField?.children[0];
@@ -90,8 +90,77 @@ const resumeUpload = async () => {
   await delay(3000);
 };
 
+const fillAllRadioType = async (applicantData: Applicant) => {
+  const allFieldset = document.querySelectorAll("fieldset");
+  if (!allFieldset || allFieldset.length === 0) {
+    return;
+  }
+  for (const fieldset of allFieldset) {
+    const legend = fieldset.querySelector("legend");
+    if (!legend) {
+      return;
+    }
+    // for gender
+    if (fromatStirngInLowerCase(legend?.textContent)?.includes("gender")) {
+      const allLabel = fieldset.querySelectorAll("label");
+      if (allLabel && allLabel.length > 0) {
+        for (const label of allLabel) {
+          if (
+            fromatStirngInLowerCase(label?.textContent) === applicantData.gender
+          ) {
+            label.click();
+          }
+        }
+      }
+    }
+    // race
+    if (fromatStirngInLowerCase(legend?.textContent)?.includes("race")) {
+      const allLabel = fieldset.querySelectorAll("label");
+      if (allLabel && allLabel.length > 0) {
+        for (const label of allLabel) {
+          if (
+            fromatStirngInLowerCase(label?.textContent)?.includes(
+              fromatStirngInLowerCase(applicantData.race)
+            )
+          ) {
+            label.click();
+          }
+        }
+      }
+    }
+  }
+};
+
+const fillDisability = async (applicantData: Applicant) => {
+  const select: HTMLSelectElement = document.querySelector(
+    "select.SelectFormField.WizardFieldInputContainer.WizardFieldInput"
+  );
+
+  if (!select) {
+    return;
+  }
+  Array.from(select.options).find((option: any) => {
+    if (
+      fromatStirngInLowerCase(option?.text)?.includes("yes") &&
+      applicantData.disability_status
+    ) {
+      option.selected = true;
+      handleValueChanges(select);
+    }
+
+    if (
+      fromatStirngInLowerCase(option?.text)?.includes("no") &&
+      !applicantData.disability_status
+    ) {
+      option.selected = true;
+      handleValueChanges(select);
+    }
+  });
+};
 export const avature = async (tempDiv: any, applicantData: Applicant) => {
-  fillEligibility(applicantData);
-  fillVisa(applicantData);
-  resumeUpload();
+  await fillEligibility(applicantData);
+  await fillVisa(applicantData);
+  await resumeUpload();
+  await fillAllRadioType(applicantData);
+  await fillDisability(applicantData);
 };
