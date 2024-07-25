b0f5a9abb4b4ecaffeb8ca832053e8e330c6e850
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-07-25 14:58:30 +0545

    linked in live

--- src/config/urlconfig.ts
+++ src/config/urlconfig.ts
@@ -4,8 +4,8 @@ export const LIVE_WEBSITE_URL = "https://app.careerai.io";
 // state handler
 // two state should be always false
 export const EXTENSION_IN_LOCAL = false;
-export const EXTENSION_IN_STAGING = true;
-export const EXTENSION_IN_LIVE = false;
+export const EXTENSION_IN_STAGING = false;
+export const EXTENSION_IN_LIVE = true;
 
 export const BASE_URL = EXTENSION_IN_LIVE
   ? "https://backend.careerai.io/api/v1"
