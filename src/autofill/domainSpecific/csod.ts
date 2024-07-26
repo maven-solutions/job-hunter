42c64e6c90e5394c883792ab82a15cab0b6d80f0
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-07-26 16:46:10 +0545

    cods added

--- src/autofill/domainSpecific/csod.ts
+++ src/autofill/domainSpecific/csod.ts
@@ -10,148 +10,190 @@ const textfieldFiller = async (applicantData: Applicant) => {
 };
 
 const selectFiller = async (applicantData: Applicant) => {
+  const allLabel = document.querySelectorAll("label");
   let veteran = false;
-  const veteranSelect: any = document.getElementById("EEOQuestion--2");
-  Array.from(veteranSelect.options).find((option: any) => {
-    //for yes
-    if (
-      applicantData.veteran_status === 1 &&
-      !veteran &&
-      (fromatStirngInLowerCase(option?.text)?.includes("amaveteran") ||
-        fromatStirngInLowerCase(option?.text)?.includes("amveteran"))
-    ) {
-      option.selected = true;
-      handleValueChanges(option);
-      veteran = true;
-    }
+  if (!allLabel || allLabel.length === 0) {
+    return;
+  }
+  for (const label of allLabel) {
+    // for ethicicy
 
     if (
-      applicantData.veteran_status === 2 &&
-      !veteran &&
-      fromatStirngInLowerCase(option?.text)?.includes("iamnot")
+      fromatStirngInLowerCase(label?.textContent)?.includes(
+        fromatStirngInLowerCase("ETHNICITY")
+      )
     ) {
-      option.selected = true;
-      handleValueChanges(option);
-      veteran = true;
-    }
+      const labelid = label.getAttribute("for");
+      if (!labelid) {
+        return;
+      }
+      const select = document.getElementById(labelid) as HTMLSelectElement;
+      if (!select) {
+        return;
+      }
+      Array.from(select.options).find((option: any) => {
+        //for yes
+        if (
+          applicantData.hispanic_or_latino &&
+          fromatStirngInLowerCase(option?.text) ===
+            fromatStirngInLowerCase("Hispanic or Latino")
+        ) {
+          option.selected = true;
+          handleValueChanges(option);
+        }
 
-    // for yes
-    if (
-      applicantData.veteran_status === 3 &&
-      !veteran &&
-      fromatStirngInLowerCase(option?.text)?.includes("identifyasaveteran")
-    ) {
-      option.selected = true;
-      handleValueChanges(option);
-      veteran = true;
+        if (
+          !applicantData.hispanic_or_latino &&
+          fromatStirngInLowerCase(option?.text) ===
+            fromatStirngInLowerCase("Not Hispanic or Latino")
+        ) {
+          option.selected = true;
+          handleValueChanges(option);
+        }
+      });
     }
+    // for veteran
+    if (
+      fromatStirngInLowerCase(label?.textContent)?.includes(
+        fromatStirngInLowerCase("VETERAN")
+      )
+    ) {
+      const labelid = label.getAttribute("for");
+      if (!labelid) {
+        return;
+      }
+      const select = document.getElementById(labelid) as HTMLSelectElement;
+      if (!select) {
+        return;
+      }
+      Array.from(select.options).find((option: any) => {
+        //for yes
+        if (
+          applicantData.veteran_status === 1 &&
+          !veteran &&
+          (fromatStirngInLowerCase(option?.text)?.includes("amaveteran") ||
+            fromatStirngInLowerCase(option?.text)?.includes("identifymyself"))
+        ) {
+          option.selected = true;
+          handleValueChanges(option);
+          veteran = true;
+        }
 
-    // for yes
-    if (
-      applicantData.veteran_status === 3 &&
-      !veteran &&
-      fromatStirngInLowerCase(option?.text)?.includes("identifyasoneormore")
-    ) {
-      option.selected = true;
-      handleValueChanges(option);
-    }
+        if (
+          applicantData.veteran_status === 2 &&
+          !veteran &&
+          fromatStirngInLowerCase(option?.text)?.includes("iamnot")
+        ) {
+          option.selected = true;
+          handleValueChanges(option);
+          veteran = true;
+        }
 
-    //for one or more
-    if (
-      applicantData.veteran_status === 4 &&
-      !veteran &&
-      fromatStirngInLowerCase(option?.text)?.includes("identifyasoneormore")
-    ) {
-      option.selected = true;
-      handleValueChanges(option);
-      veteran = true;
-    }
-    // for yes---
+        // for yes
+        if (
+          applicantData.veteran_status === 3 &&
+          !veteran &&
+          fromatStirngInLowerCase(option?.text)?.includes("identifyasaveteran")
+        ) {
+          option.selected = true;
+          handleValueChanges(option);
+          veteran = true;
+        }
 
-    if (
-      applicantData.veteran_status === 1 &&
-      !veteran &&
-      fromatStirngInLowerCase(option?.text)?.includes("identifyasoneormore")
-    ) {
-      option.selected = true;
-      handleValueChanges(option);
-      veteran = true;
-    }
+        // for yes
+        if (
+          applicantData.veteran_status === 3 &&
+          !veteran &&
+          fromatStirngInLowerCase(option?.text)?.includes("identifyasoneormore")
+        ) {
+          option.selected = true;
+          handleValueChanges(option);
+        }
 
-    //for decline
-    if (
-      applicantData.veteran_status === 5 &&
-      !veteran &&
-      (fromatStirngInLowerCase(option?.text)?.includes("selfidentify") ||
-        fromatStirngInLowerCase(option?.text)?.includes("dontwish") ||
-        fromatStirngInLowerCase(option?.text)?.includes("decline") ||
-        fromatStirngInLowerCase(option?.text)?.includes("notwish"))
-    ) {
-      option.selected = true;
-      handleValueChanges(option);
-      veteran = true;
-    }
-  });
-  const disability: any = document.getElementById("EEOQuestion--3");
-  Array.from(disability.options).find((option: any) => {
-    if (
-      fromatStirngInLowerCase(option?.text)?.includes("yes") &&
-      applicantData.disability_status
-    ) {
-      option.selected = true;
-      handleValueChanges(option);
-      return true;
-    }
+        //for one or more
+        if (
+          applicantData.veteran_status === 4 &&
+          !veteran &&
+          fromatStirngInLowerCase(option?.text)?.includes("identifyasoneormore")
+        ) {
+          option.selected = true;
+          handleValueChanges(option);
+          veteran = true;
+        }
+        // for yes---
 
-    if (
-      fromatStirngInLowerCase(option?.text)?.includes("no") &&
-      !applicantData.disability_status
-    ) {
-      option.selected = true;
-      handleValueChanges(option);
-      return true;
-    }
-  });
+        if (
+          applicantData.veteran_status === 1 &&
+          !veteran &&
+          fromatStirngInLowerCase(option?.text)?.includes("identifyasoneormore")
+        ) {
+          option.selected = true;
+          handleValueChanges(option);
+          veteran = true;
+        }
 
-  const gender: any = document.getElementById("EEOQuestion-1");
-
-  Array.from(gender.options).find((option: any) => {
-    if (
-      fromatStirngInLowerCase(option?.text) ===
-      fromatStirngInLowerCase(applicantData.gender)
-    ) {
-      option.selected = true;
-      handleValueChanges(option);
-      return true;
+        //for decline
+        if (
+          applicantData.veteran_status === 5 &&
+          !veteran &&
+          (fromatStirngInLowerCase(option?.text)?.includes("selfidentify") ||
+            fromatStirngInLowerCase(option?.text)?.includes("dontwish") ||
+            fromatStirngInLowerCase(option?.text)?.includes("decline") ||
+            fromatStirngInLowerCase(option?.text)?.includes("notwish"))
+        ) {
+          option.selected = true;
+          handleValueChanges(option);
+          veteran = true;
+        }
+      });
     }
-  });
+  }
 };
 
 const radiotypefiller = async (applicantData: Applicant) => {
-  const usawork: any = document.querySelector('[aria-labelledby="25"]');
-  if (!usawork) {
+  const allLabel = document.querySelectorAll(
+    ".p-viewSF-psqmultichoicesingleanswer"
+  );
+  if (!allLabel || allLabel.length === 0) {
     return;
   }
-  const list = usawork.children ?? "";
-  const yes: any = list[0];
-  const no: any = list[1];
-  if (applicantData.us_work_authoriztaion) {
-    yes.click();
-  }
-  if (!applicantData.us_work_authoriztaion) {
-    no.click();
+  for (const label of allLabel) {
+    //
+    const labedIdv = label.querySelector(".p-htmlviewer");
+    const labelText = labedIdv?.textContent;
+
+    if (labelText.includes("legally eligible")) {
+      const divid = labedIdv.getAttribute("id");
+      const radiogroup = label.querySelector(`[aria-labelledby="${divid}"]`);
+      const allLabelBtn = radiogroup.querySelectorAll("label");
+      if (!allLabelBtn || allLabelBtn.length === 0) {
+        return;
+      }
+
+      const yesbtn = allLabelBtn[0];
+      const nobtn = allLabelBtn[1];
+
+      if (applicantData.us_work_authoriztaion) {
+        yesbtn.click();
+        handleValueChanges(yesbtn);
+      }
+      if (!applicantData.us_work_authoriztaion) {
+        nobtn.click();
+        handleValueChanges(yesbtn);
+      }
+    }
   }
 
-  const isAdult = document.querySelector('[aria-labelledby="11"]');
-  const raidoList = isAdult.children ?? "";
-  const radioyes: any = raidoList[0];
-  const radiono: any = raidoList[1];
-  if (applicantData.is_over_18) {
-    radioyes.click();
-  }
-  if (!applicantData.is_over_18) {
-    radiono.click();
-  }
+  // const isAdult = document.querySelector('[aria-labelledby="11"]');
+  // const raidoList = isAdult.children ?? "";
+  // const radioyes: any = raidoList[0];
+  // const radiono: any = raidoList[1];
+  // if (applicantData.is_over_18) {
+  //   radioyes.click();
+  // }
+  // if (!applicantData.is_over_18) {
+  //   radiono.click();
+  // }
 };
 export const csod = async (tempDiv: any, applicantData: Applicant) => {
   await delay(3000);
