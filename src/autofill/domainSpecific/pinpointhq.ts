2d7ad9012eeaed3fe15f39444f1f1e4be7a1387c
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-09-09 16:02:29 +0545

    pinpointq updated

--- src/autofill/domainSpecific/pinpointhq.ts
+++ src/autofill/domainSpecific/pinpointhq.ts
@@ -1,11 +1,26 @@
 import { Applicant } from "../data";
-import { handleValueChanges } from "../helper";
+import { fromatStirngInLowerCase, handleValueChanges } from "../helper";
 
-export const pinpointhq = async (tempDiv: any, applicantData: Applicant) => {
+const statefiller = async (applicantData: Applicant) => {
+  const select: any = document.querySelector(
+    'select[id="application_form[application][town]"]'
+  );
+  // filling state data
+  Array.from(select.options).find((option: any) => {
+    if (
+      fromatStirngInLowerCase(option?.text) ===
+      fromatStirngInLowerCase(applicantData.state)
+    ) {
+      option.selected = true;
+      handleValueChanges(select);
+    }
+  });
+};
+
+const fillTextField = async (applicantData: Applicant) => {
   const phone: any = document.getElementById(
     "application_form_application_phone"
   );
-
   if (phone) {
     phone.value = applicantData.phone_number;
     phone.focus(); // Autofocus on the input field
@@ -13,4 +28,36 @@ export const pinpointhq = async (tempDiv: any, applicantData: Applicant) => {
     phone.select();
     handleValueChanges(phone);
   }
+
+  const adress = document.getElementById("address1") as HTMLInputElement;
+  if (adress) {
+    adress.value = applicantData.address;
+    adress.focus(); // Autofocus on the input field
+    adress.click();
+    adress.select();
+    handleValueChanges(phone);
+  }
+
+  const city = document.getElementById("address2") as HTMLInputElement;
+  if (city) {
+    city.value = applicantData.city;
+    city.focus(); // Autofocus on the input field
+    city.click();
+    city.select();
+    handleValueChanges(phone);
+  }
+
+  const postalcode = document.getElementById("postcode") as HTMLInputElement;
+  if (postalcode) {
+    postalcode.value = applicantData.zip_code.toString();
+    postalcode.focus(); // Autofocus on the input field
+    postalcode.click();
+    postalcode.select();
+    handleValueChanges(phone);
+  }
+};
+
+export const pinpointhq = async (tempDiv: any, applicantData: Applicant) => {
+  await fillTextField(applicantData);
+  await statefiller(applicantData);
 };
---
+++ src/autofill/domainSpecific/sephora.ts
@@ -0,0 +1,6 @@
+import { Applicant } from "../data";
+import { handleValueChanges } from "../helper";
+
+const selectFiller = () => {};
+
+export const sephora = async (tempDiv: any, applicantData: Applicant) => {};
