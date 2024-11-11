afa81f1e3d151b1c1234566308c6d0c2deea6e83
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-11-11 12:14:34 +0545

    saved

--- src/utils/autofillJobSavApi.ts
+++ src/utils/autofillJobSavApi.ts
@@ -2,6 +2,6 @@ import axiosInstance from "../config/axiosInstance";
 import { BASE_URL } from "../config/urlconfig";
 
 export const saveAudofillJob = async (data) => {
-  const res = await axiosInstance.post(`${BASE_URL}/autofill-jobs-o`, data);
+  const res = await axiosInstance.post(`${BASE_URL}/autofill-jobs`, data);
   return res.data;
 };
