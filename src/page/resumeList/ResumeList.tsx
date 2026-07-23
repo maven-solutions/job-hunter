cb2c866dd55fd93e4b14dd42129ae1a4a2f760af
erarunkushwaha <er.arunkushwaha@gmail.com>
2026-07-23 12:02:47 +0530

    job card added

--- src/page/resumeList/ResumeList.tsx
+++ src/page/resumeList/ResumeList.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Check, Eye } from "react-feather";
 import { RootStore, useAppDispatch, useAppSelector } from "../../store/store";
 import {
   getApplicantResume,
@@ -7,18 +6,17 @@ import {
   getDesignations,
 } from "../../store/features/ResumeList/ResumeListApi";
 import Layout from "../../template/Layout";
-import WhiteCard from "../../component/card/WhiteCard";
 import Height from "../../component/height/Height";
-import HeadingTitle from "../../component/heading/HeadingTitle";
-import AutofillFields from "./AutofillFields";
 import "./index.css";
 import "./index2.css";
-import Spinner from "../shared/Spinner";
-import { ResumeSkleton } from "../../component/skleton/Skleton";
-import IframError from "./IframError";
+import "./index.v2.css";
 import AutofillLoader from "./AutofillLoader";
 import IframeProceed from "./IframeProceed";
+import AutofillFields from "./AutofillFields";
+import ResumeListV2 from "./ResumeListV2";
 import { getApplicantSession } from "../../store/features/applicant/ApplicantApi";
+import JobCardV2 from "./JobCard.v2";
+// import { getApplicantSession } from "../../store/features/Applicant/ApplicantApi";
 
 const ResumeList = (props: any) => {
   const { setShowPage, content, autoFilling, setAutoFilling, showPage } = props;
@@ -35,48 +33,16 @@ const ResumeList = (props: any) => {
   const applicantState: any = useAppSelector((store: RootStore) => {
     return store.ApplicantSlice;
   });
+
   console.log("applicantState", applicantState);
+  console.log("resumeList", resumeList);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(getDesignations());
   }, []);
 
-  const RenderName = (props: any) => {
-    const { item } = props;
-    const role = getRoleById(
-      item?.applicant?.preferredRole,
-      item?.applicant?.customPreferredRole,
-    );
-
-    const roleString = role ? `  (${role}) ` : "";
-
-    if (item?.applicant?.title) {
-      return `${item?.applicant?.title} ${roleString}`;
-    }
-
-    if (item?.applicant?.name) {
-      return `${item?.applicant?.name} ${roleString}`;
-    }
-    return `Untitled Resume ${roleString}`;
-  };
-
-  const getRoleById = (roleiId, customRole) => {
-    if (roleiId) {
-      const role = resumeList.allRoles.filter((role) => {
-        return role.id === roleiId;
-      });
-      if (!role || role.length === 0) {
-        return null;
-      }
-      return role[0].title;
-    }
-    if (customRole) {
-      return customRole?.label;
-    }
-  };
   useEffect(() => {
-    // for organization student
     dispatch(getApplicantSession());
     if (
       authState.authenticated &&
@@ -88,89 +54,68 @@ const ResumeList = (props: any) => {
 
     if (authState.authenticated && authState?.ci_user?.userType === "va") {
       if (!resumeList.res_success || resumeList.applicantData.length === 0) {
-        // for va user
         dispatch(getApplicantsData());
       }
     } else {
-      // for normal user
       if (!resumeList.res_success) {
         dispatch(getApplicantResume(null));
       }
     }
   }, []);
 
+  const resumes = (resumeList.applicantData ?? []).map(
+    (item: any) => item.applicant ?? item,
+  );
+
+  const handlePreview = (pdfUrl: string) => {
+    window.open(pdfUrl, "_blank");
+  };
+
+  const getSessionUserName = (userId: number | undefined | null) => {
+    if (userId == null) return "";
+    const match = resumeList.applicantData?.find((user) => user.id === userId);
+    return match?.fullName ?? "";
+  };
+
   return (
     <Layout setShowPage={setShowPage} showPage={showPage} firstBgWidth="10">
-      <Height height="-10" />
-      <HeadingTitle title="Resume List" />
-      <Height height="10" />
-      {/* {iframeUrl && <IframError setIframeUrl={setIframeUrl} />} */}
+      <div className="ciautofill_v2_panel">
+        <div className="popup-content">
+          {!autoFilling && (
+            <ResumeListV2
+              loading={resumeList.loading}
+              success={resumeList.res_success}
+              resumes={resumes}
+              selectedIndex={selectedResume}
+              resumeList={resumeList}
+              onSelect={setSelectedResume}
+              onPreview={handlePreview}
+            />
+          )}
+          {autoFilling && <AutofillLoader />}
 
-      <WhiteCard>
-        {!autoFilling && (
-          <div className="ciautofill__resmelist__wrapper">
-            <Height height="-10" />
+          {/* <Height height="10" /> */}
+          {iframeUrl && <IframeProceed />}
 
-            {resumeList.res_success &&
-              resumeList.applicantData.map((item, index) => {
-                return (
-                  <div
-                    className="ciautofill_single_resume"
-                    key={item.applicant.id}
-                  >
-                    <div
-                      className="ciautofill__radio__button__section"
-                      onClick={() => setSelectedResume(index)}
-                    >
-                      {selectedResume === index && (
-                        <div className="ciautofill__radio__checked" />
-                      )}
-                    </div>
-                    <span
-                      className="ciautofill_resume_name"
-                      onClick={() => setSelectedResume(index)}
-                    >
-                      <RenderName item={item} resumeList={resumeList} />
-                    </span>
-                    {selectedResume === index && (
-                      <div className="ciautofill__checkbox__section">
-                        <Check className="ciautofill__check__icon" />
-                      </div>
-                    )}
-
-                    <div className="ext__tooltip__wrapper">
-                      <div className="ext__toolip__container">
-                        <div
-                          className="ext__toolip"
-                          onClick={() =>
-                            window.open(item.applicant.pdfUrl, "_blank")
-                          }
-                        >
-                          <Eye size={16} />
-                          <p className="ext__tooltip__text">Preview</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            {resumeList.loading && <ResumeSkleton />}
+          {applicantState.applicantSession && (
+            <JobCardV2
+              jobTitle={applicantState.applicantSession?.jobTitle}
+              userName={getSessionUserName(
+                applicantState.applicantSession?.userId,
+              )}
+            />
+          )}
+          <div className="ciautofill_v2_resume_autofill_button_section">
+            <AutofillFields
+              selectedResume={selectedResume}
+              content={content}
+              setAutoFilling={setAutoFilling}
+              setIframeUrl={setIframeUrl}
+              iframeUrl={iframeUrl}
+              autoFilling={autoFilling}
+            />
           </div>
-        )}
-        {autoFilling && <AutofillLoader />}{" "}
-      </WhiteCard>
-
-      <Height height="10" />
-      {iframeUrl && <IframeProceed />}
-      <div className="ciautofill_v2_resume_autofill_button_section">
-        <AutofillFields
-          selectedResume={selectedResume}
-          content={content}
-          setAutoFilling={setAutoFilling}
-          setIframeUrl={setIframeUrl}
-          iframeUrl={iframeUrl}
-          autoFilling={autoFilling}
-        />
+        </div>
       </div>
     </Layout>
   );
